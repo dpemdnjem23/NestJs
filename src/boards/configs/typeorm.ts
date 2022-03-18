0e56cb2e9a9de87d224d00a5fbd20036b3b43d43
gitkrak <dpemdnjem23@gmail.com>
2022-03-18 22:03:53 +0900

    type orm

---
+++ src/boards/configs/typeorm.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'test',
+  password: 'dkskqkek',
+  database: 'testuserDB',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'], //엔티티를 이용해서 db테이블 생성 그래서 엔티티 파일이 어디에 있는지 성정해준다.
+  synchronize: true,
+};
