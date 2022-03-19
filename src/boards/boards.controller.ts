88ea3b0c3663cdb4da7106624d73f58c40ce3ab5
gitkrak <dpemdnjem23@gmail.com>
2022-03-19 22:41:23 +0900

    typeorm

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -10,39 +10,50 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board.status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { Board } from './board.entity';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardService.getAllBoards();
-  }
-  //dto 적용하기
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardService.getAllBoards();
+  // }
+
   @Post()
-  //핸들러 레벨적용
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
     return this.boardService.createBoard(createBoardDto);
   }
 
+  // @Post()
+  // //핸들러 레벨적용
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardService.createBoard(createBoardDto);
+  // }
   @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
+  getBoardBuId(@Param('id') id: number): Promise<Board> {
     return this.boardService.getBoardById(id);
   }
-  @Delete('/:id')
-  deleteBoardById(@Param('id') id: string): void {
-    this.boardService.deleteBoardById(id);
-  }
-  @Patch('/:id/status')
-  updateBoardById(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ) {
-    return this.boardService.updateBoardStatus(id, status);
-  }
+
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardService.getBoardById(id);
+  // }
+  // @Delete('/:id')
+  // deleteBoardById(@Param('id') id: string): void {
+  //   this.boardService.deleteBoardById(id);
+  // }
+  // @Patch('/:id/status')
+  // updateBoardById(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ) {
+  //   return this.boardService.updateBoardStatus(id, status);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,49 +1,74 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-//id는 db에서 유니크한 값이므로 아직 db를 사용하지 않아 임시로 만들어 유니크값을준다
-import { v1 as uuid } from 'uuid';
+import { BoardStatus } from './board.status.enum';
+
 import { CreateBoardDto } from './dto/create-board.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
+
   //prvaite 를 사용하는 이유: 다른 컴포넌트에서 보드라는 배열 값을 수정을 할수 있는데,
   //그거를 차단하기 위함이다.
-  private boards: Board[] = [];
   //모든 게시물을 가져온다.
-  getAllBoards(): Board[] {
-    return this.boards; //boards배열에 들어있는 모든 값을 return(getallboards 함수 호출시)
-  }
-  //게시물 생성
-  createBoard(createBoardDto: CreateBoardDto) {
+  // getAllBoards(): Board[] {
+  //   return this.boards; //boards배열에 들어있는 모든 값을 return(getallboards 함수 호출시)
+  // }
+  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
     const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(), //유니크한 값을 게시판의 아이디로 줄수 있다.
-      title,
-      description,
+
+    const board = this.boardRepository.create({
+      title: title,
+      description: description,
       status: BoardStatus.PUBLIC,
-    };
-    this.boards.push(board);
+    });
+    await this.boardRepository.save(board);
     return board;
   }
 
-  getBoardById(id: string): Board {
-    const foundById = this.boards.find((board) => board.id === id);
+  //게시물 생성
+  // createBoard(createBoardDto: CreateBoardDto) {
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(), //유니크한 값을 게시판의 아이디로 줄수 있다.
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
+  //   this.boards.push(board);
+  //   return board;
+  // }
+  async getBoardById(id: any): Promise<Board> {
+    const found = await this.boardRepository.findOne(id);
 
-    if (!foundById) {
-      throw new NotFoundException(`Can't find Board with id${id}`);
+    if (!found) {
+      throw new NotFoundException(`can't find`);
     }
-    return foundById;
+    return found;
   }
-  //return 값을 주지 않을때 void 활용
-  deleteBoardById(id: string): void {
-    const foundById = this.getBoardById(id);
-    this.boards = this.boards.filter((board) => board.id !== foundById.id);
-  }
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    //id를 가진 보드를 먼저 찾아서 그 보드의 값을 바꿔준다.
-    //보드에있던 상태가 들어온 값으로 바뀌어야한다.
-    const getboard = this.getBoardById(id);
-    getboard.status = status;
-    return getboard;
-  }
+
+  // getBoardById(id: string): Board {
+  //   const foundById = this.boards.find((board) => board.id === id);
+  //   if (!foundById) {
+  //     throw new NotFoundException(`Can't find Board with id${id}`);
+  //   }
+  //   return foundById;
+  // }
+  // //return 값을 주지 않을때 void 활용
+  // deleteBoardById(id: string): void {
+  //   const foundById = this.getBoardById(id);
+  //   this.boards = this.boards.filter((board) => board.id !== foundById.id);
+  // }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   //id를 가진 보드를 먼저 찾아서 그 보드의 값을 바꿔준다.
+  //   //보드에있던 상태가 들어온 값으로 바뀌어야한다.
+  //   const getboard = this.getBoardById(id);
+  //   getboard.status = status;
+  //   return getboard;
+  // }
 }
